8e58ba994301c5c2d013e80bacbbdd4790204029
pixelnandez <guillemfernandezbenet@gmail.com>
2026-04-09 19:15:33 +0200

    Commit local 3

--- app/src/main/java/com/guillem/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/guillem/tic_tac_toe/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-// Hola 2, ahora es personal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
